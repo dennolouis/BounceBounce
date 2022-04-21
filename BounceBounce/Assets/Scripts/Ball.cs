2a3076bceaf281bd50d58ab4a9e1752273c49136
dennolouis <dennolouis@gmail.com>
2022-04-21 17:51:39 -0400

    added different balls

--- BounceBounce/Assets/Scripts/Ball.cs
+++ BounceBounce/Assets/Scripts/Ball.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
 
-    public float force = 50;
+    public float force = 150;
+    public float spin = 30;
+
+
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1) * force);
+        rb = GetComponent<Rigidbody>();
+        rb.AddTorque(new Vector3(0, 1, 0) * spin);
+        rb.AddForce(new Vector3(0, 0, -1) * force);
     }
 
     // Update is called once per frame
